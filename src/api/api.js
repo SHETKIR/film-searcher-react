f4dfeb0ee30dafe7b36340103ce0920fa3be84c5
shetkir <ll901mauil.ru@gmail.com>
2025-02-05 17:35:32 +0400

    done

--- src/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://www.omdbapi.com/';
-
-export async function fetchFilms(title) {
-    const response = await axios.get(`${API_URL}/?t=${title}&apikey=d3ad48a9`);
-    return response.data;
-}
--- src/components/form.js
+++ src/components/form.js
@@ -2,36 +2,35 @@ import { useState } from "react";
 import axios from "axios";
 
 const MovieSearch = () => {
-  const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
+    const [title, setTitle] = useState("");
+    const [movies, setMovies] = useState([]);
 
-  const searchMovies = async () => {
-    if (!query) return;
-    const response = await axios.get(
-        `https://www.omdbapi.com/?s=${query}&apikey=d3ad48a9`
+    const searchMovies = async (event) => {
+        event.preventDefault();
+        const response = await axios.get(`http://www.omdbapi.com/?apikey=d3ad48a9&s=${title}`);
+        setMovies(response.data.Search || []);
+    };
+
+    const handleInputChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const makeFinalList = () => {
+        return movies.map((movie) => (
+            <li key={movie.imdbID}>{movie.Title}</li>
+        ));
+    };
+
+    return (
+        <div>
+            <input value={title} onChange={handleInputChange} />
+            <button onClick={searchMovies}>Search</button>
+
+            <ul>
+                {makeFinalList()}
+            </ul>
+        </div>
     );
-    setMovies(response.data.Search || []);
-  };
-
-  return (
-    <div>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder=""
-      />
-      <button onClick={searchMovies}>Search</button>
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.imdbID}>
-            <img src={movie.Poster} alt={movie.Title} width="50" />
-            {movie.Title} ({movie.Year})
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
 };
 
 export default MovieSearch;
